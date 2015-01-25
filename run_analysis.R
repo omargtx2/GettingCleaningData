d1bf5d7ef0b64293fe9eac8093dee5e941f3f617
omargtx2 <omargtx2@yahoo.es>
2015-01-25 21:47:09 +0100

    this is my first proyect in getting and cleaning data

--- run_analysis.R
+++ run_analysis.R
@@ -1,23 +1,24 @@
-# GettingCleaningData
-this is my workspace for Getting and Cleaning Data course
 
-Luigi Omar Torres
-Enero, 2015
 
-Getting and Cleaning Data Course
+#You should create one R script called run_analysis.R that does the following. 
+
+# 1. Merges the training and the test sets to create one data set.
+# 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+# 3. Uses descriptive activity names to name the activities in the data set
+# 4. Appropriately labels the data set with descriptive variable names. 
+# 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+if(!file.exists("./data")){dir.create("./data")}
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(fileUrl,destfile="./data/Dataset.zip",method="curl")
+
+
+unzip("./data/Dataset.zip",exdir="./data")
 
 path_rf <- file.path("./data" , "UCI HAR Dataset")
 files<-list.files(path_rf, recursive=TRUE)
 files
 
-
-Firstly, I download the file, in the path using this command
-
-fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-download.file(fileUrl,destfile="./data/Dataset.zip",method="curl")
-
-but in this case I got an error, so I downloaded before in this file "data"
-here I describe the differents ways to get the result and necessary data, to manipulate the data and I get the tidy file
 
 #Read the Activity files
 
@@ -75,4 +76,5 @@ aggr.data <- aggregate(data.mean[, 3:ncol(data.mean)],
 
 write.table(format(aggr.data, scientific=T), "tidyFinal1.txt",
             row.names=F, col.names=F, quote=2)
+
 
